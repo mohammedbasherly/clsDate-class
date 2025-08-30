185cc282178146ce63532c4978a30e5a807d5370
Mohammed Basher <101946147+mohammedbasherly@users.noreply.github.com>
2025-08-30 14:55:06 +0200

    Add files via upload

---
+++ clsDate Library/clsDate Library/clsDate Library.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+#include "C:/Programming/Libraries/clsDate.h"
+
+#include "C:/Programming/Libraries/library.h"
+
+int main()
+{
+
+	clsDate date1;
+	date1.print(true);
+
+	clsDate date2("10/10/2001");
+	date2.setDate(1, 1, 2009);
+	cout << date2.isDateEqualToDate2({1, 1, 2222}) << endl;
+	date2.print(true);
+
+	clsDate date3(10, 10, 2001);
+	date3.print(true);
+
+	cout << "Date 3:\n";
+	date3.printMonthCalendar();
+
+	return 0;
+}
